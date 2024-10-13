55592aa64196822797b09a246ba5c77c689e7e61
Aishwarya Ghorpade <96725124+AishwaryaGhorpade@users.noreply.github.com>
2024-10-13 13:22:49 +0530

    Delete Basics directory

--- Basics/02PropsEx/src/Components/Greet.jsx
+++
@@ -1,7 +0,0 @@
-import React,{Component} from "react";
-class Greet extends Component{
-    render(){
-        return <h4>Hello {this.props.username}</h4>
-    }
-}
-export default Greet
--- Basics/02PropsEx/src/Components/Hello.jsx
+++
@@ -1,7 +0,0 @@
-// import React from 'recat'
-const Hello=({name="Yash",age="5"})=>{  //defalt name is Yash,deafalt age 5
-    return(
-        <h1>Hello {name} your age is {age}</h1>
-    )
-}
-export default Hello
